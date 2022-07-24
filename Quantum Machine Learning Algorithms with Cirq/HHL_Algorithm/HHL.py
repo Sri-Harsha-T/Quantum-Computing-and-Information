e5471630f6d4a3fdfb4f0fa97085191a6750b1b5
Sri-Harsha-T <srihari.thotaharsha@gmail.com>
2022-07-24 06:51:25 +0530

    QML

---
+++ Quantum Machine Learning Algorithms with Cirq/HHL_Algorithm/HHL.py
@@ -0,0 +1,32 @@
+import cirq
+
+class HamiltonianSimulation(cirq.EigenGate, cirq.SingleQubitGate):
+ """
+ This class simulates the Hamiltonian evolution for
+ a Single qubit. For a Hamiltonian given by H the
+ Unitary Operator simulated for time t is
+ given by e**(-iHt). An Eigenvalue of lambda for the
+ Hamiltonian H corresponds to the
+ Eigenvalue of e**(-i*lambda*t).
+ The EigenGate takes in an Eigenvalue of the
+ form e**(i*pi*theta) as theta and the corresponding Eigenvector 
+as |v><v|
+ """
+ def __init__(self, _H_, t, exponent=1.0):
+    cirq.SingleQubitGate.__init__(self)
+    cirq.EigenGate.__init__(self, exponent=exponent)
+    self._H_ = _H_
+    self.t = t
+    eigen_vals, eigen_vecs = np.linalg.eigh(self._H_)
+    self.eigen_components = []
+    for _lambda_, vec in zip(eigen_vals, eigen_vecs.T):
+        theta = -_lambda_*t / np.pi
+        _proj_ = np.outer(vec, np.conj(vec))
+        self.eigen_components.append((theta, _proj_))
+
+
+ def _with_exponent(self, exponent):
+    return HamiltonianSimulation(self._H_, self.t, exponent)
+
+ def _eigen_components(self):
+    return self.eigen_components
